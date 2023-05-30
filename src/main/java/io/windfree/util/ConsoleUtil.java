f37b969b7a31ef0510ab83a46c6ca336427c10fd
iwindfree <knamkim>
2023-05-30 23:52:12 +0900

    add console util

--- src/main/java/io/windfree/util/ConsoleUtil.java
+++ src/main/java/io/windfree/util/ConsoleUtil.java
@@ -8,7 +8,16 @@ public class ConsoleUtil {
         String input = scanner.nextLine();
         System.out.println("input = " + input);
     }
+    
+    public static void readConsole2() {
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        do{
+            input = scanner.nextLine();
+            System.out.println("input = " + input);
+        } while(!input.equalsIgnoreCase("q"));
+    }
     public static void main(String[] args) {
-        ConsoleUtil.readConsole();
+        ConsoleUtil.readConsole2();
     }
 }
